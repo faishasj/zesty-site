873704ae669e510511b0a18ab0905a507b2dd112
Derek Huang <derek.huang@monash.edu>
2024-08-16 17:01:47 +1000

    Init Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div className="h-screen w-screen flex flex-col items-center justify-center bg-slate-800">
+			<article className="text-3xl font-bold text-white">
+				HERE WE GO
+			</article>
+		</div>
+	)
+}
+
+export default App
