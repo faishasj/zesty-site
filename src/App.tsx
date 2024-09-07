2f632df3071c9a122456a22ae465c7407809b145
Faisha Surjatin <faisha@faisha.dev>
2024-09-07 20:42:42 +1000

    Add maxRedirects to axios get config

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import axios from "axios"
-import { useEffect } from "react"
+import axios from "axios";
+import { useEffect } from "react";
 
 function App() {
-	useEffect(() => {
-		axios
-			.get(
-				"https://script.google.com/macros/s/AKfycbxK1fkBOqEto5kRs5EmVOnaCfQfgIvkH_BaRpwMo_rt-r6DkaIogeKqlq5H-O01AMJp8g/exec"
-			)
-			.then((output) => {
-				console.log(output)
-			})
-	}, [])
+  useEffect(() => {
+    axios
+      .get(
+        "https://script.google.com/macros/s/AKfycbxRyBIHoFmybe_xCHm6yGEI5PXA-1Ls8tGnfXY9QsFMRGPKNA1qQi6SRQNbBSfMPtKyAw/exec",
+        {
+          maxRedirects: 0,
+        }
+      )
+      .then((output) => {
+        console.log(output);
+      });
+  }, []);
 
-	return (
-		<div className="h-screen w-screen flex flex-col items-center justify-center bg-slate-700">
-			<article className="text-3xl font-bold text-white">
-				HERE WE GO
-			</article>
-			<button className="btn btn-primary">Primary</button>
-		</div>
-	)
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center bg-slate-700">
+      <article className="text-3xl font-bold text-white">HERE WE GO</article>
+      <button className="btn btn-primary">Primary</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
