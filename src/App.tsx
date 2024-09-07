7149db4173f2cdac8c1a964b85a10e84ed9864c7
Derek Huang <derek.huang@monash.edu>
2024-09-07 20:07:19 +1000

    Added call to google script

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,17 @@
+import axios from "axios"
+import { useEffect } from "react"
+
 function App() {
+	useEffect(() => {
+		axios
+			.get(
+				"https://script.google.com/macros/s/AKfycbxK1fkBOqEto5kRs5EmVOnaCfQfgIvkH_BaRpwMo_rt-r6DkaIogeKqlq5H-O01AMJp8g/exec"
+			)
+			.then((output) => {
+				console.log(output)
+			})
+	}, [])
+
 	return (
 		<div className="h-screen w-screen flex flex-col items-center justify-center bg-slate-700">
 			<article className="text-3xl font-bold text-white">
