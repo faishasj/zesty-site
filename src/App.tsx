22e2d1eee57eae827c897471ef869fb3c2ba9e6e
Derek Huang <derek.huang@monash.edu>
2024-09-07 18:57:00 +1000

    base themeing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 function App() {
 	return (
-		<div className="h-screen w-screen flex flex-col items-center justify-center bg-slate-800">
+		<div className="h-screen w-screen flex flex-col items-center justify-center bg-slate-700">
 			<article className="text-3xl font-bold text-white">
 				HERE WE GO
 			</article>
+			<button className="btn btn-primary">Primary</button>
 		</div>
 	)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,8 @@ export default {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	daisyui: {
+		themes: ["dark", { test: { primary: "#ffffff" } }],
+	},
+	plugins: [require("daisyui")],
 }
