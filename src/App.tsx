4948694888fa97f6f1cf8a054915e92f7e5e52aa
Derek Huang <derek.huang@monash.edu>
2024-09-07 22:37:05 +1000

    :3

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import axios from "axios";
-import { useEffect } from "react";
+import axios from "axios"
+import { useEffect } from "react"
 
 function App() {
-  useEffect(() => {
-    axios
-      .get(
-        "https://script.google.com/macros/s/AKfycbxRyBIHoFmybe_xCHm6yGEI5PXA-1Ls8tGnfXY9QsFMRGPKNA1qQi6SRQNbBSfMPtKyAw/exec",
-        {
-          maxRedirects: 0,
-        }
-      )
-      .then((output) => {
-        console.log(output);
-      });
-  }, []);
+	useEffect(() => {
+		axios
+			.post(
+				"https://script.google.com/macros/s/AKfycbxRyBIHoFmybe_xCHm6yGEI5PXA-1Ls8tGnfXY9QsFMRGPKNA1qQi6SRQNbBSfMPtKyAw/exec",
+				{
+					maxRedirects: 0,
+					params: {
+						actionType: "lookup",
+						query: "John",
+					},
+				}
+			)
+			.then((output) => {
+				console.log(output)
+			})
+	}, [])
 
-  return (
-    <div className="h-screen w-screen flex flex-col items-center justify-center bg-slate-700">
-      <article className="text-3xl font-bold text-white">HERE WE GO</article>
-      <button className="btn btn-primary">Primary</button>
-    </div>
-  );
+	return (
+		<div className="h-fit w-screen flex flex-col items-center bg-primary">
+			<div className="p-4 h-screen flex flex-col items-center">
+				<article className="text-xl font-bold text-white">
+					The Wedding
+				</article>
+				<article className="text-lg font-bold text-white">of</article>
+				<article className="text-3xl font-bold text-white">
+					Nicholas and Rebekah
+				</article>
+				<article className="text-3xl font-bold text-white">
+					Somewhere
+				</article>
+			</div>
+			<div></div>
+		</div>
+	)
 }
 
-export default App;
+export default App
