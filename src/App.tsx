4c9c369761ed27f7a93008ce53a933dd4f62ffb8
Faisha Surjatin <faisha@faisha.dev>
2024-09-07 23:43:21 +1000

    Change post to get request

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,37 @@
-import axios from "axios"
-import { useEffect } from "react"
+import axios from "axios";
+import { useEffect } from "react";
 
 function App() {
-	useEffect(() => {
-		axios
-			.post(
-				"https://script.google.com/macros/s/AKfycbxRyBIHoFmybe_xCHm6yGEI5PXA-1Ls8tGnfXY9QsFMRGPKNA1qQi6SRQNbBSfMPtKyAw/exec",
-				{
-					maxRedirects: 0,
-					params: {
-						actionType: "lookup",
-						query: "John",
-					},
-				}
-			)
-			.then((output) => {
-				console.log(output)
-			})
-	}, [])
+  useEffect(() => {
+    axios
+      .get(
+        "https://script.google.com/macros/s/AKfycbyM-9QSmfJV5fq7D2f39B7ZAC6qGz8MpbY4YzElXK9aWMPjmqsR8r3Esh-aD6ly5ZawuQ/exec",
+        {
+          maxRedirects: 0,
+          params: {
+            actionType: "lookup",
+            query: "john",
+          },
+        }
+      )
+      .then((output) => {
+        console.log(output);
+      });
+  }, []);
 
-	return (
-		<div className="h-fit w-screen flex flex-col items-center bg-primary">
-			<div className="p-4 h-screen flex flex-col items-center">
-				<article className="text-xl font-bold text-white">
-					The Wedding
-				</article>
-				<article className="text-lg font-bold text-white">of</article>
-				<article className="text-3xl font-bold text-white">
-					Nicholas and Rebekah
-				</article>
-				<article className="text-3xl font-bold text-white">
-					Somewhere
-				</article>
-			</div>
-			<div></div>
-		</div>
-	)
+  return (
+    <div className="h-fit w-screen flex flex-col items-center bg-primary">
+      <div className="p-4 h-screen flex flex-col items-center">
+        <article className="text-xl font-bold text-white">The Wedding</article>
+        <article className="text-lg font-bold text-white">of</article>
+        <article className="text-3xl font-bold text-white">
+          Nicholas and Rebekah
+        </article>
+        <article className="text-3xl font-bold text-white">Somewhere</article>
+      </div>
+      <div></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
