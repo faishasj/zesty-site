5e0702aba04a76e13a05df8a7991975486abd4fd
Michelle Chen <michelle.chen@monash.edu>
2024-10-20 00:05:27 +1100

    change image extension type

--- src/Images/images.ts
+++ src/Images/images.ts
@@ -3,8 +3,8 @@ const IMAGES = {
   chevronDown: new URL("./chevronDown.png", import.meta.url).href,
   image1: new URL("./image1.jpg", import.meta.url).href,
   image2: new URL("./image2.jpeg", import.meta.url).href,
-  image3: new URL("./image3.jpg", import.meta.url).href,
-  image4: new URL("./image4.jpg", import.meta.url).href,
+  image3: new URL("./image3.JPG", import.meta.url).href,
+  image4: new URL("./image4.JPG", import.meta.url).href,
   collage: new URL("./collage.png", import.meta.url).href,
   seeYouThere: new URL("./seeYouThere.png", import.meta.url).href,
 };
