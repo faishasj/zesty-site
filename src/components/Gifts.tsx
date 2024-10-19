7aad39f495c87499eb7979bba2757aae7c17d34d
Michelle Chen <michelle.chen@monash.edu>
2024-10-19 23:22:55 +1100

    gift page

--- src/components/Gifts.tsx
+++ src/components/Gifts.tsx
@@ -2,22 +2,56 @@ import IMAGES from "../Images/images";
 
 function Gifts() {
   return (
-    <div className="flex h-screen w-full items-center justify-center snap-start bg-primary">
-      <div className="h-full w-full object-cover overflow-hidden">
-        <article className="flex h-full">
-          <div className="h-full w-6/12 bg-info object-cover overflow-hidden">
-            <div>
+    <div className="flex h-screen w-full snap-start bg-primary">
+        <div className="flex items-center justify-center h-full w-1/2 bg-info">
+          <div className="flex flex-col w-2/3 gap-y-7">
+            <article className="text-4xl font-semibold>">Gifts</article>
+            <div className="flex flex-col gap-y-5">
+              <article className="text-lg">
+              Should you wish to honour us with a gift, a contribution
+              towards our future together would be sincerely
+              appreciated.
+              </article>
+              <div className="flex flex-col">
+                <article className="text-2xl font-medium">Bank Transfer</article>
+                <div className="inline-flex gap-x-2">
+                  <article className="text-lg font-semibold">ACC Name:</article>
+                  <article className="text-lg">Lorum Ipsum</article>
+                </div>
+                <div className="inline-flex gap-x-2">
+                  <article className="text-lg font-semibold">BSB No:</article>
+                  <article className="text-lg">201 234</article>
+                </div>
+                <div className="inline-flex gap-x-2">
+                  <article className="text-lg font-semibold">ACC No:</article>
+                  <article className="text-lg">0264 855 878</article>
+                </div>
+              </div>
+
+              <div className="flex flex-col">
+                <article className="text-2xl font-medium">Alternative payments:</article>
+                <div className="inline-flex gap-x-2">
+                  <article className="text-lg font-semibold">Beem:</article>
+                  <article className="text-lg">@limnicholased</article>
+                </div>
+                <div className="inline-flex gap-x-2">
+                  <article className="text-lg font-semibold">PayID:</article>
+                  <article className="text-lg">0481290817</article>
+                </div>
+              
+              </div>
+              <article className="text-lg">Please write “Wedding Gift from name” in the reference section so that we can thank you after the wedding.</article>
 
             </div>
+           
           </div>
-          <img
-            className="h-full w-6/12 object-cover overflow-hidden"
-            src={IMAGES.image1}
-            alt="Photo collage of Rebekah and Nicholas."
-          />
-        </article>
-        
-      </div>
+          
+        </div>
+        <img
+          className="h-full w-1/2 object-cover overflow-hidden"
+          src={IMAGES.image1}
+          alt="Photo collage of Rebekah and Nicholas."
+        />
     </div>
   );
 }
