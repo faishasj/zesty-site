e503491179995179fadec137ade351d9f147c968
Derek Huang <derek.huang@monash.edu>
2024-10-19 22:01:18 +1100

    FAQ text

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,47 @@
-import IMAGES from "./Images/images";
-import FaqComp from "./components/FaqComp";
-import Gifts from "./components/Gifts";
-import RsvpForm from "./components/RsvpForm";
-import Schedule from "./components/Schedule";
-import Travel from "./components/Footer";
-import Welcome from "./components/Welcome";
-import Footer from "./components/Footer";
+import IMAGES from "./Images/images"
+import FaqComp from "./components/FaqComp"
+import Gifts from "./components/Gifts"
+import RsvpForm from "./components/RsvpForm"
+import Schedule from "./components/Schedule"
+import Travel from "./components/Footer"
+import Welcome from "./components/Welcome"
+import Footer from "./components/Footer"
 
 function App() {
-  return (
-    <div className="w-screen h-screen items-center bg-b overflow-y-auto snap-y snap-mandatory">
-      {/* Page 1 - Welcome */}
-      <Welcome />
-      {/* Page 2 - Image */}
-      <div className="h-screen w-full snap-start snap-always">
-        <img
-          className="h-full w-full object-cover overflow-hidden"
-          src={IMAGES.image1}
-          alt="first image"
-        />
-      </div>
-      {/* Page 3 - Schedule */}
-      <Schedule />
-      {/* Page 4 - Gifts */}
-      <Gifts />
-      {/* Page 5 - Travel */}
-      <Travel />
-      {/* Page 6 - FAQ */}
-      <FaqComp />
-      {/* Page 7 - RSVP */}
-      <RsvpForm />
-      {/* Page 8 - Collage */}
-      <div className="h-screen w-full snap-start snap-always">
-        <img
-          className="h-full w-full object-cover overflow-hidden"
-          src={IMAGES.collage}
-          alt="Photo collage of Rebekah and Nicholas."
-        />
-      </div>
-      {/* Page 9 - Footer */}
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="w-screen h-screen items-center bg-white overflow-y-auto snap-y snap-mandatory">
+			{/* Page 1 - Welcome */}
+			<Welcome />
+			{/* Page 2 - Image */}
+			<div className="h-screen w-full snap-start snap-always">
+				<img
+					className="h-full w-full object-cover overflow-hidden"
+					src={IMAGES.image1}
+					alt="first image"
+				/>
+			</div>
+			{/* Page 3 - Schedule */}
+			<Schedule />
+			{/* Page 4 - Gifts */}
+			<Gifts />
+			{/* Page 5 - Travel */}
+			<Travel />
+			{/* Page 6 - FAQ */}
+			<FaqComp />
+			{/* Page 7 - RSVP */}
+			<RsvpForm />
+			{/* Page 8 - Collage */}
+			<div className="h-screen w-full snap-start snap-always">
+				<img
+					className="h-full w-full object-cover overflow-hidden"
+					src={IMAGES.collage}
+					alt="Photo collage of Rebekah and Nicholas."
+				/>
+			</div>
+			{/* Page 9 - Footer */}
+			<Footer />
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/components/FaqComp.tsx
+++ src/components/FaqComp.tsx
@@ -9,8 +9,36 @@ function FaqComp() {
 					Will the ceremony be outside?
 				</article>
 				<article>
-					Yes! Please bring a light jacket just in case there is
-					weather on the day
+					Yes! Please bring a light jacket just in case there is rainy
+					weather on the day.
+				</article>
+				<article>The reception will be indoors.</article>
+				<div className="divider divider-secondary"></div>
+				<article className="text-xl">What is the dress code?</article>
+				<article>
+					Formal dress code, autumnal pinks, greens, red, oranges.
+				</article>
+				<div className="flex mt-2 h-8 gap-x-2">
+					<div className="w-8 rounded-md bg-base-100" />
+					<div className="w-8 rounded-md bg-neutral" />
+					<div className="w-8 rounded-md bg-secondary" />
+					<div className="w-8 rounded-md bg-info" />
+				</div>
+				<div className="divider divider-secondary"></div>
+				<article className="text-xl">Will children be allowed?</article>
+				<article>Yes</article>
+				<div className="divider divider-secondary"></div>
+				<article className="text-xl">
+					Looking into accomodation options?
+				</article>
+				<article>Rydges Hotel</article>
+				<div className="divider divider-secondary"></div>
+				<article className="text-xl">
+					Just attending the ceremony?
+				</article>
+				<article>
+					Yes? Please bring a light jacket in case there is weather on
+					the day?
 				</article>
 				<div className="divider divider-secondary"></div>
 			</div>
