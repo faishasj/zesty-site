808cc4f6cfbf26c97cd8fefa4bf61b8a68f88565
Derek Huang <derek.huang@monash.edu>
2024-10-19 22:30:15 +1100

    base travel page

--- src/components/Travel.tsx
+++ src/components/Travel.tsx
@@ -1,25 +1,17 @@
-import IMAGES from "../Images/images";
+import IMAGES from "../Images/images"
 
 function Travel() {
-    return (
-        <div className="flex h-screen w-full items-center justify-center snap-start bg-primary">
-            <div className="h-full w-full object-cover overflow-hidden">
-                <article className="flex h-full">
-                    <img
-                        className="h-full w-6/12 object-cover overflow-hidden"
-                        src={IMAGES.image1}
-                        alt="Photo collage of Rebekah and Nicholas."
-                    />
-                    <div className="h-full w-6/12 bg-base-300 object-cover overflow-hidden">
-                        <div>
-
-                        </div>
-                    </div>
-
-                </article>
-
-            </div>
-        </div>
-    );
+	return (
+		<div className="flex h-screen w-full snap-start bg-primary">
+			<img
+				className="h-full w-1/2 object-cover overflow-hidden"
+				src={IMAGES.image1}
+				alt="Photo collage of Rebekah and Nicholas."
+			/>
+			<div className="h-full w-1/2 bg-base-300">
+				<article>Travel</article>
+			</div>
+		</div>
+	)
 }
-export default Travel;
+export default Travel
