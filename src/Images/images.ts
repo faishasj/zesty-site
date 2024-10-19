862cebee2b73a42b95a787816334f533b2960c25
Faisha Surjatin <faisha@faisha.dev>
2024-10-19 22:02:23 +1100

    Add font + welcome page

--- src/Images/images.ts
+++ src/Images/images.ts
@@ -1,4 +1,6 @@
 const IMAGES = {
+  title: new URL("./title.png", import.meta.url).href,
+  chevronDown: new URL("./chevronDown.png", import.meta.url).href,
   image1: new URL("./image1.jpg", import.meta.url).href,
   collage: new URL("./collage.png", import.meta.url).href,
 };
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,11 +1,22 @@
+import IMAGES from "../Images/images";
+
 function Welcome() {
   return (
-    <div className="flex flex-col items-center p-4 h-screen w-full bg-primary snap-start snap-always">
-      <article className="text-xl font-bold text-white">The Wedding</article>
-      <article className="text-lg font-bold text-white">of</article>
-      <article className="text-3xl font-bold text-white">
-        Nicholas and Rebekah
+    <div className="flex flex-col gap-20 items-center justify-center p-4 h-screen w-full bg-base-content snap-start snap-always">
+      <img
+        className="max-w-2xl"
+        src={IMAGES.title}
+        alt="Nicholas and Rebekah are getting married!"
+      />
+      <article className="text-xl text-black font-medium">
+        4:30 in the afternoon | 12th of April | RYDGES Melbourne
       </article>
+      <div className="flex flex-col items-center">
+        <article className="text-xl text-black font-semibold">
+          SCROLL DOWN TO RSVP
+        </article>
+        <img className=" w-[24px] h-[24px]" src={IMAGES.chevronDown} />
+      </div>
     </div>
   );
 }
