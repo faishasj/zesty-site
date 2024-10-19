ff6e9493dde7dabb0cd0f10bfba96964fd04bd17
Derek Huang <derek.huang@monash.edu>
2024-10-19 22:38:11 +1100

    travel page

--- src/components/Travel.tsx
+++ src/components/Travel.tsx
@@ -8,8 +8,22 @@ function Travel() {
 				src={IMAGES.image1}
 				alt="Photo collage of Rebekah and Nicholas."
 			/>
-			<div className="h-full w-1/2 bg-base-300">
-				<article>Travel</article>
+			<div className="flex items-center justify-center h-full w-1/2 bg-base-300">
+				<div className="flex flex-col w-2/3 gap-y-4">
+					<article className="text-4xl font-semibold">Travel</article>
+					<article className="text-xl">
+						Should you wish to honour us with a gift, a contribution
+						towards our future together would be sincerely
+						appreciated.
+					</article>
+					<article className="text-xl">
+						See attached PDF{" "}
+						<a className="cursor-pointer" href="">
+							<u>here</u>
+						</a>
+						.
+					</article>
+				</div>
 			</div>
 		</div>
 	)
