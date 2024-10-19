8108db53ef8da3b14ef6e3b89f5d0b44d7cc7575
Derek Huang <derek.huang@monash.edu>
2024-10-19 22:24:21 +1100

    faq page done

--- src/components/FaqComp.tsx
+++ src/components/FaqComp.tsx
@@ -1,8 +1,8 @@
 function FaqComp() {
 	return (
-		<div className="flex h-screen w-full items-center justify-center snap-start bg-primary">
-			<div className="flex flex-col items-center w-3/5">
-				<article className="text-5xl mb-12">
+		<div className="flex h-screen w-full items-center justify-center snap-start bg-base-content">
+			<div className="flex flex-col items-center w-3/5 text-black">
+				<article className="text-4xl mb-12 font-semibold">
 					Frequently asked questions
 				</article>
 				<article className="text-2xl">
@@ -13,7 +13,7 @@ function FaqComp() {
 					weather on the day.
 				</article>
 				<article>The reception will be indoors.</article>
-				<div className="divider divider-secondary"></div>
+				<div className="divider divider-warning"></div>
 				<article className="text-2xl">What is the dress code?</article>
 				<article>
 					Formal dress code, autumnal pinks, greens, red, oranges.
@@ -24,17 +24,17 @@ function FaqComp() {
 					<div className="w-8 rounded-md bg-secondary" />
 					<div className="w-8 rounded-md bg-info" />
 				</div>
-				<div className="divider divider-secondary"></div>
+				<div className="divider divider-warning"></div>
 				<article className="text-2xl">
 					Will children be allowed?
 				</article>
 				<article>Yes</article>
-				<div className="divider divider-secondary"></div>
+				<div className="divider divider-warning"></div>
 				<article className="text-2xl">
 					Looking into accomodation options?
 				</article>
 				<article>Rydges Hotel</article>
-				<div className="divider divider-secondary"></div>
+				<div className="divider divider-warning"></div>
 				<article className="text-2xl">
 					Just attending the ceremony?
 				</article>
@@ -42,7 +42,7 @@ function FaqComp() {
 					Yes? Please bring a light jacket in case there is weather on
 					the day?
 				</article>
-				<div className="divider divider-secondary"></div>
+				<div className="divider divider-warning"></div>
 			</div>
 		</div>
 	)
